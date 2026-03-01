05c087a9c72bd792239282bfef25c274abc2e55c
FixvolvV <neskawy222@gmail.com>
2026-03-02 00:00:57 +0600

    Fix project StartUp

--- src/shoersshopapi/core/settings.py
+++ src/shoersshopapi/core/settings.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 class RunConfig(BaseModel):
     mode: str = "development"
@@ -11,18 +14,21 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
+class Httpcors(BaseModel):
+    urls: list = []
+
 class Settings(BaseSettings):
-    
-    config = SettingsConfigDict (
-        env_file=(".env", ".env.template"),
+    run: RunConfig = RunConfig()
+    db: DatabaseConfig
+    httpcors: Httpcors = Httpcors()
+
+    model_config = SettingsConfigDict (
+        env_file=(BASE_DIR / ".env", BASE_DIR / ".env.template"),
         env_nested_delimiter="__",
-        env_prefix="API_",
+        env_prefix="API__",
         case_sensitive=False,
         env_file_encoding="utf-8",
         extra="ignore",
     )
 
-    run: RunConfig = RunConfig()
-    db: DatabaseConfig = DatabaseConfig() #pyright: ignore
-
-settings = Settings()
+settings = Settings() #pyright: ignore
