43cbcbca191f1b47efa896a673751bb4535a147b
FixvolvV <neskawy222@gmail.com>
2026-03-01 23:49:07 +0600

    Configure startup init.

---
+++ src/shoersshopapi/__init__.py
@@ -0,0 +1,29 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from shoersshopapi.core.settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # startup
+    yield
+    # shutdown
+
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url=None if settings.run.mode == "production" else "/docs", #disables docs
+    redoc_url=None if settings.run.mode == "production" else "/redoc", #disables redoc
+    openapi_url=None if settings.run.mode == "production" else "/openapi.json", #disables openapi.json suggested by tobias comment.
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.httpcors.urls, #pyright:ignore
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
