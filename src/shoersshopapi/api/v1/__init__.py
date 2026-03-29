a2436fa3394fbb6a624a9e020770c420f00e7b46
FixvolvV <neskawy222@gmail.com>
2026-03-29 14:55:11 +0600

    Add routers to app

--- src/shoersshopapi/api/v1/__init__.py
+++ src/shoersshopapi/api/v1/__init__.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
 
 from shoersshopapi.api.v1.users.controller import router as users_router
+from shoersshopapi.api.v1.brands.controller import router as brands_router
+from shoersshopapi.api.v1.products.controller import router as products_router
+from shoersshopapi.api.v1.orders.controller import router as orders_router
+from shoersshopapi.api.v1.carts.controller import router as carts_router
+from shoersshopapi.api.v1.addresses.controller import router as addresses_router
 
+from shoersshopapi.api.v1.streams.controller import router as streams_router
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(users_router, prefix="/users")
+router.include_router(brands_router, prefix="/brands")
+router.include_router(products_router, prefix="/products")
+router.include_router(orders_router, prefix="/orders")
+router.include_router(carts_router, prefix="/carts")
+router.include_router(addresses_router, prefix="/addresses")
+
+router.include_router(streams_router, prefix="/streams")
