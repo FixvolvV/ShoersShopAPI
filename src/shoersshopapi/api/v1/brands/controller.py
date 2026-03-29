f9327c2bd69db6e1e72f1ec6b2cd3b45a98f7561
FixvolvV <neskawy222@gmail.com>
2026-03-29 14:54:39 +0600

    Add and Fix brands controller

---
+++ src/shoersshopapi/api/v1/brands/controller.py
@@ -0,0 +1,124 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Query, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from shoersshopapi.core.database import database
+from .crud import BrandCrud
+from shoersshopapi.api.v1.schemas import BrandWithId, BrandFilter
+
+
+router = APIRouter(tags=["Brands"])
+
+BRANDNOTFOUND = HTTPException(
+    status_code=404,
+    detail="Brand not found"
+)
+
+@router.post(
+    "/",
+    response_model=BrandWithId,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_brand(
+    session: Annotated[
+        AsyncSession,
+        Depends(database.get_session)
+    ],
+    name: str,
+    logo: Annotated[
+        UploadFile,
+        File(...)
+    ]
+):
+    brand = await BrandCrud.create_brand(session, brand_name=name, brand_logo=logo)
+    return brand
+
+
+@router.get(
+    "/{brand_id}",
+    response_model=BrandWithId,
+    summary="Получить бренд по ID",
+)
+async def get_brand(
+    session: Annotated[
+        AsyncSession,
+        Depends(database.get_session)
+    ],
+    brand_id: str,
+):
+    brand = await BrandCrud.get_by_id(session, brand_id)
+
+    if not brand:
+        raise BRANDNOTFOUND
+
+    return brand
+
+
+@router.get(
+    "/",
+    response_model=list[BrandWithId],
+    summary="Получить список брендов",
+)
+async def get_brands(
+    session: Annotated[
+        AsyncSession,
+        Depends(database.get_session)
+    ],
+    page: Annotated[int, Query(ge=1)] = 1,
+    limit: Annotated[int, Query(ge=1, le=100)] = 20,
+):
+    brands = await BrandCrud.get_all(
+        session,
+        limit=limit,
+        offset=(page - 1) * limit,
+    )
+    return brands
+
+
+@router.patch(
+    "/{brand_id}",
+    response_model=BrandWithId,
+)
+async def update_brand_logo(
+    session: Annotated[
+        AsyncSession,
+        Depends(database.get_session)
+    ],
+    brand_id: str,
+    brand_name: str | None,
+    logo: Annotated[
+        UploadFile | None,
+        File(...)
+    ]
+):
+
+    if not brand_name and not logo:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Нужно передать хотя бы одно поле для обновления",
+        )
+
+    brand = await BrandCrud.update_brand(session, brand_id, brand_name, logo)
+
+    if not brand:
+        raise BRANDNOTFOUND
+
+    return brand
+
+
+@router.delete(
+    "/{brand_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_brand(
+    session: Annotated[
+        AsyncSession,
+        Depends(database.get_session)
+    ],
+    brand_id: str,
+):
+    deleted = await BrandCrud.delete_brand(session, brand_id)
+
+    if not deleted:
+        raise BRANDNOTFOUND
---
+++ src/shoersshopapi/api/v1/brands/crud.py
@@ -0,0 +1,116 @@
+from typing import Union, Sequence
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import UploadFile
+
+from shoersshopapi.api.v1.basecrud import BaseCrud
+from shoersshopapi.api.v1.utils import gen_uuid
+
+from shoersshopapi.core.settings import settings
+from shoersshopapi.core.database.models import Brand
+from shoersshopapi.core.minio.image_service import image_service
+
+from shoersshopapi.api.v1.schemas import BrandWithId, BrandFilter, BrandUpdate
+
+
+class BrandCrud(BaseCrud[Brand]):
+    model = Brand
+
+    FOLDER = settings.minio.bucket_name
+
+    # === CREATE ===
+
+    @classmethod
+    async def create_brand(
+        cls,
+        session: AsyncSession,
+        brand_name: str,
+        brand_logo: UploadFile,
+    ) -> Union[Brand, None]:
+        logo_path = await image_service.upload_image(brand_logo, cls.FOLDER)
+
+        data = BrandWithId(
+            id=gen_uuid(),
+            brand_name=brand_name,
+            brand_logo=logo_path,
+        )
+
+        brand = await cls.add(session, data)
+        await session.commit()
+
+        return brand 
+
+    # === READ ===
+
+    @classmethod
+    async def get_by_id(cls, session: AsyncSession, brand_id: str):
+        stmt = cls.stmt().filters(BrandFilter(id=brand_id)).build()
+        return await cls.find_one_or_none(session, stmt)
+
+    @classmethod
+    async def get_all(
+        cls,
+        session: AsyncSession,
+        filters: BrandFilter | None = None,
+        limit: int = 20,
+        offset: int = 0,
+    ):
+        stmt = (
+            cls.stmt()
+            .filters(filters)
+            .limit(limit)
+            .offset(offset)
+            .build()
+        )
+        return await cls.find_all(session, stmt)
+
+    # === UPDATE ===
+
+    @classmethod
+    async def update_brand(
+        cls,
+        session: AsyncSession,
+        brand_id: str,
+        brand_name: str | None = None,
+        logo: UploadFile | None = None,
+    ):
+        brand = await cls.find_one_or_none_by_id(session, brand_id)
+
+        if not brand:
+            return None
+
+        update_data = BrandUpdate()
+
+        if logo:
+            new_logo_path = await image_service.replace_image(
+                old_path=brand.brand_logo,
+                file=logo,
+                folder=cls.FOLDER,
+            )
+            update_data.brand_logo = new_logo_path
+
+        if brand_name:
+            update_data.brand_name = brand_name
+
+        brand = await cls.update_one_by_id(
+            session, brand_id, update_data
+        )
+        await session.commit()
+
+        return brand
+
+
+    # === DELETE ===
+
+    @classmethod
+    async def delete_brand(cls, session: AsyncSession, brand_id: str) -> bool:
+        brand = await cls.find_one_or_none_by_id(session, brand_id)
+
+        if not brand:
+            return False
+
+        await image_service.delete_image(brand.brand_logo)
+        brand = await cls.delete_one_by_id(session, brand_id)
+        await session.commit()
+        
+        return brand
