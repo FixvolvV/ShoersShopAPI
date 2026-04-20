7f821537501ceca62acfe93ee9186a6a57d64e98
FixvolvV <neskawy222@gmail.com>
2026-04-20 19:51:36 +0600

    Add unique constraint from favorite model

--- src/shoersshopapi/core/database/models/favorite.py
+++ src/shoersshopapi/core/database/models/favorite.py
@@ -4,8 +4,13 @@ from shoersshopapi.core.database.models.mixin import (
 )
 from .base import Base
 
+from sqlalchemy import UniqueConstraint
+
 class Favorite(UserRelationMixin, ProductRelationMixin, Base):
     _user_back_populates = "favorites"
     _user_load_strategy = "raise"
     _product_back_populates = "favorites"
 
+    __table_args__ = (
+            UniqueConstraint('user_id', 'product_id', name='uq_user_product_favorite'),
+        )
